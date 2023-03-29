4612120440d55cdb180137bdeefe0ced46808e2b
Mavis Hou <h.siqian@wustl.edu>
2023-03-29 17:53:32 -0500

    save psql sensitive data to env

--- backend_api/__init__.py
+++ backend_api/__init__.py
@@ -1,5 +1,9 @@
 import flask
+import os
 app = flask.Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mavis:757496457@localhost/one_billion_db"
+psql_username = os.environ['PSQL_USERNAME']
+psql_password = os.environ['PSQL_PASSWORD']
+psql_uri = "postgresql://" + psql_username + ":" + psql_password + "@localhost/one_billion_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = psql_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'demo_secrets'
